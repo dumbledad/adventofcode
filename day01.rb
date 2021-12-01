46c8d51bb43f6696093bc593d1e8b767e82e4712
Tim Regan <tim.regan@raspberrypi.org>
2021-12-01 14:35:26 +0000

    Does sliding window

--- day01.rb
+++ day01.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 depth_readings = File.readlines('day01-input-01.txt').map(&:to_i)
-accumulated = depth_readings.reduce([0, 0]) do |accumulator, depth|
-  [accumulator[0] + (depth > accumulator[1] ? 1 : 0), depth]
+
+def num_increases(input_array)
+  accumulated = input_array.reduce([0, 0]) do |accumulator, value|
+    [accumulator[0] + (value > accumulator[1] ? 1 : 0), value]
+  end
+  accumulated[0] - 1
 end
-puts accumulated[0] - 1
+
+puts "Depth increase count: #{num_increases depth_readings}"
+
+three_sum_depth_readings = depth_readings.each_cons(3).to_a.map(&:sum)
+puts "3-sum depth increase count: #{num_increases three_sum_depth_readings}"
