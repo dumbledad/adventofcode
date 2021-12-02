aae3819dca67ac33060583e8d8ac4c476e5cc46f
Tim Regan <tim.regan@raspberrypi.org>
2021-12-02 06:47:51 +0000

    Using aim for depth

--- day02.rb
+++ day02.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
-course_readings = File.readlines('day02-input-01.txt').map do |line| 
+course_readings = File.readlines('day02-input-01.txt').map do |line|
   split_line = line.split
   [split_line[0], split_line[1].to_i]
 end
+
+##########
+# PART 1 #
+##########
 
 def sum_commands(command, input_array)
   input_array.reduce(0) do |accumulation, command_value|
@@ -17,3 +21,24 @@ up = sum_commands('up', course_readings)
 depth = down - up
 
 puts "Our new position is #{forward} forward at #{depth} depth, which multiply to give #{forward * depth}"
+
+##########
+# PART 2 #
+##########
+
+aim = 0
+depth = 0
+horizontal_position = 0
+course_readings.each do |command, value|
+  case command
+  when 'forward'
+    horizontal_position += value
+    depth += aim * value
+  when 'up'
+    aim -= value
+  when 'down'
+    aim += value
+  end
+end
+
+puts "Our new position is (#{depth}, #{horizontal_position}) i.e. #{depth * horizontal_position}"
