df90399e248786f9f8f2d6f867c875a055ab31d3
Tim Regan <tim.regan@raspberrypi.org>
2021-12-02 06:51:46 +0000

    Adding part comments to day 1

--- day01.rb
+++ day01.rb
@@ -6,7 +6,15 @@ def num_increases(input_array)
   input_array.each_cons(2).reduce(0) { |count, values| count + (values[1] > values[0] ? 1 : 0) }
 end
 
+##########
+# PART 1 #
+##########
+
 puts "Depth increase count: #{num_increases depth_readings}"
+
+##########
+# PART 2 #
+##########
 
 three_sum_depth_readings = depth_readings.each_cons(3).map(&:sum)
 puts "3-sum depth increase count: #{num_increases three_sum_depth_readings}"
