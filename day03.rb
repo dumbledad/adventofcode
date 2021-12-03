c6ead5b326920d3eaf6a59ace28dd2541e2ce5a5
Tim Regan <tim.regan@raspberrypi.org>
2021-12-03 09:23:31 +0000

    Moving count_digits to function

--- day03.rb
+++ day03.rb
@@ -1,27 +1,33 @@
 # frozen_string_literal: true
 
-diagnostic_length = File.open('day03-input-01.txt', &:readline).chomp().length
+DigitCounts = Struct.new(:one_counts, :zero_counts)
+
+DIAGNOSTIC_LENGTH = File.open('day03-input-01.txt', &:readline).chomp().length
 diagnostics = File.readlines('day03-input-01.txt').map { |reading| reading.to_i(2) }
+
+# rubocop:disable Metrics/MethodLength
+def count_digits(input_diagnostics_array)
+  digit_counts = DigitCounts.new(Array.new(DIAGNOSTIC_LENGTH, 0), Array.new(DIAGNOSTIC_LENGTH, 0))
+  (0...DIAGNOSTIC_LENGTH).each do |i|
+    input_diagnostics_array.each do |diagnostic|
+      if ((diagnostic >> i) & 1) == 1
+        digit_counts.one_counts[i] += 1
+      else
+        digit_counts.zero_counts[i] += 1
+      end
+    end
+  end
+  digit_counts
+end
+# rubocop:enable Metrics/MethodLength
 
 ##########
 # PART 1 #
 ##########
 
-DigitCounts = Struct.new(:one_counts, :zero_counts)
-digit_counts = DigitCounts.new(Array.new(diagnostic_length, 0), Array.new(diagnostic_length, 0))
-
-(0...diagnostic_length).each do |i|
-  diagnostics.each do |diagnostic|
-    if ((diagnostic >> i) & 1) == 1
-      digit_counts.one_counts[i] += 1
-    else
-      digit_counts.zero_counts[i] += 1
-    end
-  end
-end
-
 gamma_str = epsilon_str = ''
-(0...diagnostic_length).each do |i|
+digit_counts = count_digits(diagnostics)
+(0...DIAGNOSTIC_LENGTH).each do |i|
   if digit_counts.one_counts[i] > digit_counts.zero_counts[i]
     gamma_str = "1#{gamma_str}"
     epsilon_str = "0#{epsilon_str}"
@@ -32,3 +38,7 @@ gamma_str = epsilon_str = ''
 end
 
 puts "the power consumption of the submarine is #{gamma_str.to_i(2) * epsilon_str.to_i(2)}"
+
+##########
+# PART 2 #
+##########
