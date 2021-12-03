545a71ed4f7a026da7687f0342f9262f284c08c1
Tim Regan <tim.regan@raspberrypi.org>
2021-12-03 14:38:23 +0000

    yuck, but it worked

--- day03.rb
+++ day03.rb
@@ -43,38 +43,67 @@ puts "the power consumption of the submarine is #{gamma_str.to_i(2) * epsilon_st
 # PART 2 #
 ##########
 
-counts = digit_counts
-oxygen_candidates = diagnostics
-(0...DIAGNOSTIC_LENGTH).each do |i|
-  if counts.one_counts[i] >= counts.zero_counts[i]
-    oxygen_candidates = oxygen_candidates.select do |diagnostic|
-      diagnostic & (((2**(DIAGNOSTIC_LENGTH - i)) - 1)) >= (2**(DIAGNOSTIC_LENGTH - (i + 1)))
-    end
-  else
-    oxygen_candidates = oxygen_candidates.select do |diagnostic|
-      diagnostic & (((2**(DIAGNOSTIC_LENGTH - i)) - 1)) < (2**(DIAGNOSTIC_LENGTH - (i + 1)))
+# counts = digit_counts
+# oxygen_candidates = diagnostics
+# (0...DIAGNOSTIC_LENGTH).each do |i|
+#   if counts.one_counts[i] >= counts.zero_counts[i]
+#     oxygen_candidates = oxygen_candidates.select do |diagnostic|
+#       diagnostic & (((2**(DIAGNOSTIC_LENGTH - i)) - 1)) >= (2**(DIAGNOSTIC_LENGTH - (i + 1)))
+#     end
+#   else
+#     oxygen_candidates = oxygen_candidates.select do |diagnostic|
+#       diagnostic & (((2**(DIAGNOSTIC_LENGTH - i)) - 1)) < (2**(DIAGNOSTIC_LENGTH - (i + 1)))
+#     end
+#   end
+#   break if oxygen_candidates.length == 1
+
+#   counts = count_digits(oxygen_candidates)
+# end
+
+# counts = digit_counts
+# scrubber_candidates = diagnostics
+# (0...DIAGNOSTIC_LENGTH).each do |i|
+#   if counts.one_counts[i] < counts.zero_counts[i]
+#     scrubber_candidates = scrubber_candidates.select do |diagnostic|
+#       diagnostic & (((2**(DIAGNOSTIC_LENGTH - i)) - 1)) >= (2**(DIAGNOSTIC_LENGTH - (i + 1)))
+#     end
+#   else
+#     scrubber_candidates = scrubber_candidates.select do |diagnostic|
+#       diagnostic & (((2**(DIAGNOSTIC_LENGTH - i)) - 1)) < (2**(DIAGNOSTIC_LENGTH - (i + 1)))
+#     end
+#   end
+#   break if scrubber_candidates.length == 1
+
+#   counts = count_digits(scrubber_candidates)
+# end
+
+# puts "oxygen (#{oxygen_candidates[0]}) * oxygen (#{scrubber_candidates[0]}) = #{oxygen_candidates[0] * scrubber_candidates[0]}"
+
+# Let me try brute force in text :-(
+
+diagnostic_strings = File.readlines('day03-input-01.txt')
+oxygen_strings = scrubber_strings = diagnostic_strings
+
+(0...12).each do |i|
+  if oxygen_strings.length > 1
+    ones_count = oxygen_strings.count { |d| d[i] == '1' }
+    zeros_count = oxygen_strings.count { |d| d[i] == '0' }
+    if ones_count >= zeros_count
+      oxygen_strings = oxygen_strings.select { |d| d[i] == '1' }
+    else
+      oxygen_strings = oxygen_strings.select { |d| d[i] == '0' }
     end
   end
-  break if oxygen_candidates.length == 1
-
-  counts = count_digits(oxygen_candidates)
-end
-
-counts = digit_counts
-scrubber_candidates = diagnostics
-(0...DIAGNOSTIC_LENGTH).each do |i|
-  if counts.one_counts[i] < counts.zero_counts[i]
-    scrubber_candidates = scrubber_candidates.select do |diagnostic|
-      diagnostic & (((2**(DIAGNOSTIC_LENGTH - i)) - 1)) >= (2**(DIAGNOSTIC_LENGTH - (i + 1)))
-    end
-  else
-    scrubber_candidates = scrubber_candidates.select do |diagnostic|
-      diagnostic & (((2**(DIAGNOSTIC_LENGTH - i)) - 1)) < (2**(DIAGNOSTIC_LENGTH - (i + 1)))
+  if scrubber_strings.length > 1
+    ones_count = scrubber_strings.count { |d| d[i] == '1' }
+    zeros_count = scrubber_strings.count { |d| d[i] == '0' }
+    if ones_count < zeros_count
+      scrubber_strings = scrubber_strings.select { |d| d[i] == '1' }
+    else
+      scrubber_strings = scrubber_strings.select { |d| d[i] == '0' }
     end
   end
-  break if scrubber_candidates.length == 1
-
-  counts = count_digits(scrubber_candidates)
 end
-
-puts "oxygen (#{oxygen_candidates[0]}) * oxygen (#{scrubber_candidates[0]}) = #{oxygen_candidates[0] * scrubber_candidates[0]}"
+puts oxygen_strings
+puts scrubber_strings
+puts oxygen_strings[0].to_i(2) * scrubber_strings[0].to_i(2)
