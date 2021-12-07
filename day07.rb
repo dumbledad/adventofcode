b4e43c3ac89087dced9ad80dbbafde5f23ae5549
Tim Regan <tim.regan@raspberrypi.org>
2021-12-07 05:59:28 +0000

    Wrong answer to Part 2

--- day07.rb
+++ day07.rb
@@ -6,29 +6,38 @@
 class Crabs
   attr_accessor :horizontals, :fuel_costs
 
-  def initialize(input_data_filename)
+  def initialize(input_data_filename, increasing)
     @horizontals = File.open(input_data_filename, &:readline).chomp.split(',').map(&:to_i)
-    set_fuel_costs
-  end
-
-  def set_fuel_costs
     @fuel_costs = {}
     (@horizontals.min...@horizontals.max).each do |target|
-      @fuel_costs[target] = fuel_to target
+      @fuel_costs[target] = fuel_to(target, increasing)
     end
   end
 
-  def fuel_to(horizontal)
-    @horizontals.reduce(0) { |sum, h| sum + (horizontal - h).magnitude }
+  def fuel_to(horizontal, increasing)
+    return @horizontals.reduce(0) { |sum, h| sum + (horizontal - h).magnitude } unless increasing
+
+    @horizontals.reduce(0) { |sum, h| sum + (1...(horizontal - h).magnitude).sum }
   end
 
   def min_fuel_cost
     cost = [@fuel_costs.min_by { |_, f| f }].first
-    { :horizontal => cost[0], :fuel => cost[1] }
+    { horizontal: cost[0], fuel: cost[1] }
   end
 end
 
+puts "\nPART ONE"
 puts "\nTest dataset:"
-crabs = Crabs.new('day07-input-test.txt')
-# puts "It would take #{crabs.fuel_to crabs.average} units of fuel to get to their average horizontal position"
-puts crabs.min_fuel_cost[:fuel]
+crabs = Crabs.new('day07-input-test.txt', false)
+puts "It would take #{crabs.min_fuel_cost[:fuel]} units of fuel to get to #{crabs.min_fuel_cost[:horizontal]}"
+puts "\nTest dataset:"
+crabs = Crabs.new('day07-input-01.txt', false)
+puts "It would take #{crabs.min_fuel_cost[:fuel]} units of fuel to get to #{crabs.min_fuel_cost[:horizontal]}"
+puts "\nPART TWO"
+puts "\nTest dataset:"
+crabs = Crabs.new('day07-input-test.txt', true)
+puts "It would take #{crabs.min_fuel_cost[:fuel]} units of fuel to get to #{crabs.min_fuel_cost[:horizontal]}"
+puts "\nTest dataset:"
+crabs = Crabs.new('day07-input-01.txt', true)
+puts "It would take #{crabs.min_fuel_cost[:fuel]} units of fuel to get to #{crabs.min_fuel_cost[:horizontal]}"
+puts ''
