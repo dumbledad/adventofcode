ce09145a0d73562a90f5be645e4d36193a899475
Tim Regan <tim.regan@raspberrypi.org>
2021-12-07 08:23:40 +0000

    Solved Part 2
    But I don't get why we need to add 1 to the magnitude

--- day07.rb
+++ day07.rb
@@ -17,7 +17,8 @@ class Crabs
   def fuel_to(horizontal, increasing)
     return @horizontals.reduce(0) { |sum, h| sum + (horizontal - h).magnitude } unless increasing
 
-    @horizontals.reduce(0) { |sum, h| sum + (1...(horizontal - h).magnitude).sum }
+    # adding one to the magnitude in the next line makes no sense to me :-(
+    @horizontals.reduce(0) { |sum, h| sum + (1...((horizontal - h).magnitude + 1)).sum }
   end
 
   def min_fuel_cost
