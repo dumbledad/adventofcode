f68cdabb458b732618fcb9b4727bde43601697bf
Tim Regan <tim.regan@raspberrypi.org>
2021-12-07 05:39:36 +0000

    Test data working

--- day07.rb
+++ day07.rb
@@ -4,21 +4,31 @@
 
 # All the crabs
 class Crabs
-  attr_accessor :horizontals
+  attr_accessor :horizontals, :fuel_costs
 
   def initialize(input_data_filename)
     @horizontals = File.open(input_data_filename, &:readline).chomp.split(',').map(&:to_i)
+    set_fuel_costs
   end
 
-  def average
-    @horizontals.sum / @horizontals.length
+  def set_fuel_costs
+    @fuel_costs = {}
+    (@horizontals.min...@horizontals.max).each do |target|
+      @fuel_costs[target] = fuel_to target
+    end
   end
 
   def fuel_to(horizontal)
     @horizontals.reduce(0) { |sum, h| sum + (horizontal - h).magnitude }
   end
+
+  def min_fuel_cost
+    cost = [@fuel_costs.min_by { |_, f| f }].first
+    { :horizontal => cost[0], :fuel => cost[1] }
+  end
 end
 
 puts "\nTest dataset:"
 crabs = Crabs.new('day07-input-test.txt')
-puts "It would take #{crabs.fuel_to crabs.average} units of fuel to get to their average horizontal position"
+# puts "It would take #{crabs.fuel_to crabs.average} units of fuel to get to their average horizontal position"
+puts crabs.min_fuel_cost[:fuel]
