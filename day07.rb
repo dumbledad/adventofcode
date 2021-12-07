fd3c916575dd930d1b3ed61a3fad7da75c7b9ddb
Tim Regan <tim.regan@raspberrypi.org>
2021-12-07 05:23:56 +0000

    Using average does not work

--- day07.rb
+++ day07.rb
@@ -1,3 +1,24 @@
 # frozen_string_literal: true
 
 # https://adventofcode.com/2021/day/7
+
+# All the crabs
+class Crabs
+  attr_accessor :horizontals
+
+  def initialize(input_data_filename)
+    @horizontals = File.open(input_data_filename, &:readline).chomp.split(',').map(&:to_i)
+  end
+
+  def average
+    @horizontals.sum / @horizontals.length
+  end
+
+  def fuel_to(horizontal)
+    @horizontals.reduce(0) { |sum, h| sum + (horizontal - h).magnitude }
+  end
+end
+
+puts "\nTest dataset:"
+crabs = Crabs.new('day07-input-test.txt')
+puts "It would take #{crabs.fuel_to crabs.average} units of fuel to get to their average horizontal position"
