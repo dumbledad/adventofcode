650d806a409df1c2add0c971a7f4a8409058a741
Tim Regan <tim.regan@raspberrypi.org>
2021-12-10 20:34:01 +0000

    Part 2

--- day10.rb
+++ day10.rb
@@ -8,7 +8,7 @@ class Checker
   def initialize(input_data_filename)
     @loc = File.readlines(input_data_filename).map(&:chomp)
     @illegal_char_scores = { ')' => 3, ']' => 57, '}' => 1_197, '>' => 25_137, false => 0 }
-    @autocomplete_char_scores = { ')' => 1, ']' => 2, '}' => 3, '>' => 4, false => 0 }
+    @autocomplete_char_scores = { ')' => 1, ']' => 2, '}' => 3, '>' => 4}
     @opening_brackets = ['(', '[', '{', '<']
     @opening_to_closing = { '(' => ')', '[' => ']', '{' => '}', '<' => '>' }
     @closing_brackets = [')', ']', '}', '>']
@@ -34,6 +34,35 @@ class Checker
   def total_syntax_error_score
     loc.reduce(0) { |score, line| score + illegal_char_scores[illegal(line)] }
   end
+
+  def autocomplete(line)
+    unclosed_brackets = []
+    line.chars.each do |c|
+      if opening_brackets.include? c
+        unclosed_brackets << c
+      else
+        return [] unless unclosed_brackets[-1] == closing_to_opening[c]
+
+        unclosed_brackets.pop
+      end
+    end
+    unclosed_brackets.map { |c| opening_to_closing[c] }.reverse
+  end
+
+  def autocomplete_score(line)
+    score = 0
+    autocomplete(line).each do |c|
+      score *= 5
+      score += autocomplete_char_scores[c]
+    end
+    score
+  end
+
+  # Part 2
+  def middle_autocomplete_score
+    scores = loc.map { |line| autocomplete_score(line) }.select(&:positive?).sort
+    scores[scores.length / 2]
+  end
 end
 
 puts "\nPART ONE"
@@ -43,3 +72,11 @@ puts "The total syntax error score is #{data.total_syntax_error_score}"
 puts "\nFull dataset:"
 data = Checker.new('day10-input-01.txt')
 puts "The total syntax error score is #{data.total_syntax_error_score}"
+
+puts "\nPART TWO"
+puts "\nTest dataset:"
+data = Checker.new('day10-input-test.txt')
+puts "The middle autocomplete score is #{data.middle_autocomplete_score}"
+puts "\nFull dataset:"
+data = Checker.new('day10-input-01.txt')
+puts "The middle autocomplete score is #{data.middle_autocomplete_score}"
