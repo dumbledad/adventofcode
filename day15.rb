b0a45f5c43e081e53f8e925aded9c4794818b1dd
Tim Regan <tim.regan@raspberrypi.org>
2021-12-15 05:40:12 +0000

    Seeding simple path

--- day15.rb
+++ day15.rb
@@ -1,13 +1,38 @@
 # frozen_string_literal: true
 
-# https://adventofcode.com/2021/day/14
-class OptimalPolymerFormula
-  attr_accessor :template, :polymer, :insertion_map
+# https://adventofcode.com/2021/day/15
+class Chitons
+  attr_accessor :risks, :paths, :i_length, :j_length, :lowest_risk, :lowest_risk_path
 
   def initialize(input_data_filename)
-    @template = File.open(input_data_filename, &:readline).chomp
-    @pair_counts = (0...@template.length - 1).each_with_object(Hash.new(0)) { |i, h| h[@template[i, 2]] += 1 }
-    @insertion_map = initialize_insertions(
-      File.readlines(input_data_filename).drop(2).map(&:chomp).map { |l| l.split(' -> ') }
-    )
+    @risks = File.readlines(input_data_filename).map(&:chomp).map(&:chars).map { |r| r.map(&:to_i) }
+    @paths = []
+    @i_length = @risks.length
+    @j_length = @risks[0].length
+    add_initial_path
   end
+
+  # First path is straight down from top left and then accross the bottom
+  def add_initial_path
+    path = []
+    (0...@i_length).each { |i| path << [[i, 0], @risks[i][0]] }
+    (0...@j_length).each { |j| path << [[0, j], @risks[0][j]] }
+    @lowest_risk_path = path
+    @lowest_risk = risk(path)
+    @paths << path
+  end
+
+  # Calculate a path's risk (omitting the top left starting point as we leave it)
+  def risk(path)
+    (1...path.length).reduce(0) do |sum, i|
+      sum + path[i][1]
+    end
+  end
+end
+
+def path(filename)
+  data = Chitons.new(filename)
+  puts "The lowest risk path has #{data.lowest_risk} risk (#{filename})"
+end
+
+['day15-input-test.txt'].each { |f| path(f) }
