9a1abcf0d3c8cfe415d35a0a540de50890e0fa1d
Tim Regan <tim.regan@raspberrypi.org>
2021-12-16 07:48:59 +0000

    WiP

--- day16.rb
+++ day16.rb
@@ -10,15 +10,31 @@ class Transmission
 
   def print_transmissions
     @transmissions.each do |t|
-      puts t.inspect(2)
+      puts t.to_s(2)
     end
   end
 
-  # def parse_version(bin)
-  # end
+  def parse_packet_from(num)
+    version = num >> (num.bit_length - 3)
+    num & (1 << (num.bit_length - 3))
+  end
 
-  # def parse_type_id(bin)
-  # end
+  def drop_top_bits(drop_count, num)
+    ones = (1 << drop_count) - 1
+    num & (ones << (num.bit_length - drop_count))
+  end
+
+  def parse_version(bin_str)
+    parse_n(3, bin_str)
+  end
+
+  def parse_type_id(bin_str)
+    parse_n(3, bin_str)
+  end
+
+  def parse_n(n, bin_str)
+    [bin_str[0, n].to_i(2), bin_str[n..]]
+  end
 end
 
 def path(filename)
@@ -27,7 +43,7 @@ def path(filename)
   # puts "The lowest risk path has #{data.dijkstra} risk (#{filename}#{msg})"
 end
 
-['day16-input-test.txt', 'day16-input-01.txt'].each { |f| path(f) }
+'day16-input-test.txt', 'day16-input-01.txt'].each { |f| path(f) }
 
 # 16
 # 12
