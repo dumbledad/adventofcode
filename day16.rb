12396b852cb682f88625df4220463083de9432e6
Tim Regan <tim.regan@raspberrypi.org>
2021-12-16 06:56:56 +0000

    Printing the binary

--- day16.rb
+++ day16.rb
@@ -2,25 +2,34 @@
 
 # https://adventofcode.com/2021/day/16
 class Transmission
-  attr_accessor :risks, :i_length, :j_length
+  attr_accessor :transmissions
 
-  def initialize(input_data_filename, expand: false)
-    @risks = File.readlines(input_data_filename).map(&:chomp).map(&:chars).map { |r| r.map(&:to_i) }
-    @risks = expand_risks if expand
-    @i_length = @risks.length
-    @j_length = @risks[0].length
+  def initialize(input_data_filename)
+    @transmissions = File.readlines(input_data_filename).map(&:chomp).map { |h| h.unpack1('H*').to_i }
   end
+
+  def print_transmissions
+    @transmissions.each do |t|
+      puts t.inspect(2)
+    end
+  end
+
+  # def parse_version(bin)
+  # end
+
+  # def parse_type_id(bin)
+  # end
 end
 
-def path(filename, expand)
-  data = Chitons.new(filename, expand: expand)
-  msg = expand ? ' expanded' : ''
-  puts "The lowest risk path has #{data.dijkstra} risk (#{filename}#{msg})"
+def path(filename)
+  data = Transmission.new(filename)
+  data.print_transmissions
+  # puts "The lowest risk path has #{data.dijkstra} risk (#{filename}#{msg})"
 end
 
-[false, true].each { |e| ['day15-input-test.txt', 'day15-input-01.txt'].each { |f| path(f, e) } }
+['day16-input-test.txt', 'day16-input-01.txt'].each { |f| path(f) }
 
-16
-12
-23
-31
+# 16
+# 12
+# 23
+# 31
