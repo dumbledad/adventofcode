50edfc268d185a269bcf9d619189fa59f4760b58
Tim Regan <tim.regan@raspberrypi.org>
2021-12-18 12:43:09 +0000

    WiP using arrays and extensions
    But exploding will be hard. Perhaps back to strings :-(

--- day18.rb
+++ day18.rb
@@ -1,29 +1,42 @@
 # frozen_string_literal: true
 
-module ToSN
+require 'json'
+
+module Refinements
+  refine Array do
+    def depth
+      map { |element| element.depth + 1 }.max
+    end
+  end
+
+  refine Object do
+    def depth
+      0
+    end
+  end
+
   refine String do
-    def to_sn
-      # No-op.
+    def to_sn(sn_string)
+      JSON.parse(sn_string)
     end
   end
 end
 
 # https://adventofcode.com/2021/day/18
 class SnailfishNumber
-  using ToSN
+  using Refinements
 
-  attr_accessor :pair, :parent
-
-  def initialize(parent)
-    @pair = [0, 0]
-    @parent = parent
+  def self.add(array_a, array_b)
+    SnailfishNumber.reduce [array_a, array_b]
   end
 
-  def +(other)
-    add(other)
-  end
+  def self.reduced(sn_array, nesting: 0)
+    return true if sn_array.depth <= 4
 
-  def add(other)
-    # No-op.
+    sn_array.each do |sna|
+      return false if sna.is_a? Numeric && sna < 10
+      if sna.is_a? Numeric
+      end
+
   end
 end
--- spec/day18_spec.rb
+++ spec/day18_spec.rb
@@ -1,8 +1,28 @@
 # frozen_string_literal: true
 
 require './day18'
+using ToSN
 
 # https://adventofcode.com/2021/day/17
+RSpec.describe String do
+  describe '#to_sn' do
+    it 'correctly parses a snailfish number string' do
+      sn_string = '[[1,2],3]'
+      sn = sn_string.to_sn
+      expect(sn.pair[0].parent).to eq(sn)
+      expect(sn.pair[1].parent).to eq(sn)
+      expect(sn.pair[0].pair[0].parent).to eq(sn.pair[0])
+      expect(sn.pair[0].pair[1].parent).to eq(sn.pair[0])
+      expect(sn.pair[1].pair).to be_nil
+      expect(sn.pair[1].number).to eq(3)
+      expect(sn.pair[0].pair[0].pair).to be_nil
+      expect(sn.pair[0].pair[0].number).to eq(1)
+      expect(sn.pair[0].pair[1].pair).to be_nil
+      expect(sn.pair[0].pair[1].number).to eq(2)
+    end
+  end
+end
+
 RSpec.describe SnailfishNumber do
   description = 'target area: x=20..30, y=-10..-5'
   describe '#parse_target_description' do
