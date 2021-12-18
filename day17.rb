10c3f04f28272d48172491c7e153415b667c3860
Tim Regan <tim.regan@raspberrypi.org>
2021-12-18 06:06:40 +0000

    WiP

--- day17.rb
+++ day17.rb
@@ -2,23 +2,88 @@
 
 # https://adventofcode.com/2021/day/17
 class Probe
-  attr_accessor :start, :trajectory, :result, :max_y_reached
+  attr_accessor :start, :position, :initial_direction, :current_direction, :target, :trajectory, :hit, :max_y_reached
+
+  def initialize(starting_point, direction, target)
+    @start = starting_point.clone
+    @position = starting_point.clone
+    @max_y_reached = starting_point[1]
+    @initial_direction = direction.clone
+    @current_direction = direction.clone
+    @target = target
+    @hit = false
+    plot_trajectory
+  end
+
+  def plot_trajectory
+    @trajectory = [@start]
+    while keep_going?
+      @position[0] = @position[0] + @current_direction[0]
+      @position[1] = @position[1] + @current_direction[1]
+      @trajectory << @position.clone
+      @max_y_reached = @position[1] if @position[1] > @max_y_reached
+      @current_direction[0] = [0, @current_direction[0] - 1].max
+      @current_direction[1] = @current_direction[1] - 1
+    end
+  end
+
+  def keep_going?
+    return false if @position[0] > @target[1] || @position[1] > @target[3]
+
+    if in_target?
+      @hit = true
+      return false
+    end
+    true
+  end
+
+  def in_target?
+    @position[0] >= @target[0] && @position[0] <= @target[1] && @position[1] >= @target[2] && @position[1] <= @target[3]
+  end
+
+  def visualize_trajectory
+    y_start = [0, @max_y_reached].max
+    grid = Array.new(1 + y_start - @target[2]) { |_| Array.new(@target[1] + 1, '.') }
+    (@target[0]..target[1]).each do |x|
+      (@target[2]..@target[3]).each do |y|
+        grid[(y - y_start).magnitude][x] = 'T'
+      end
+    end
+    (0..@target[1]).each do |x|
+      (@target[2]..y_start).each do |y|
+        grid[(y - y_start).magnitude][x] = '*' if @trajectory.include? [x, y]
+      end
+    end
+    grid.each { |row| puts row.join }
+    grid.map(&:join)
+  end
 end
 
 class ProbeLauncher
   attr_accessor :target, :probes
 
-  def initialize
+  def initialize(target_description)
+    @target = parse_target_description(target_description)
+    calculate_probes
+  end
+
+  def calculate_probes
+    # Needn't go below 45 degrees nor beyond the extent of the target
     @probes = []
+    (1..10).each do |y_mag|
+      (1..10).each do |x_mag|
+        @probes << Probe.new([0, 0], [x_mag, y_mag], @target)
+      end
+    end
   end
 
   # E.g. 'target area: x=20..30, y=-10..-5' becomes [20, 30, -10, -5]
   def parse_target_description(description)
-    /x=([-\d.]*).*y=([-\d.]*)/.match(description).captures.map { |e| e.split('..').map { |i| i.to_i }}.flatten(1)
+    /x=([-\d.]*).*y=([-\d.]*)/.match(description).captures.map { |e| e.split('..').map(&:to_i) }.flatten(1)
   end
 
   def max_y_reached
-    @probes.max { |a, b| a.max_y_reached <=> b.max_y_reached }.max_y_reached
+    @probes.select(&:hit).max { |a, b| a.max_y_reached <=> b.max_y_reached }.max_y_reached
   end
 end
 
--- spec/day17_spec.rb
+++ spec/day17_spec.rb
@@ -7,19 +7,35 @@ RSpec.describe ProbeLauncher do
   description = 'target area: x=20..30, y=-10..-5'
   describe '#parse_target_description' do
     it 'correctly parses the target description string into [x_min, x_max, y_min, y_max] array' do
-      launcher = ProbeLauncher.new
-      target = launcher.parse_target_description(description)
-      expect(target).to eq([20, 30, -10, -5])
+      launcher = ProbeLauncher.new(description)
+      expect(launcher.target).to eq([20, 30, -10, -5])
     end
   end
 
   describe '#max_y_reached' do
     it 'correctly finds the maxumum y possible while still hitting target' do
-      launcher = ProbeLauncher.new
-      target = launcher.parse_target_description(description)
-      launcher.target = target
+      launcher = ProbeLauncher.new(description)
       max_y = launcher.max_y_reached
       expect(max_y).to eq(45)
+    end
+  end
+end
+
+RSpec.describe Probe do
+  describe '#visualize_trajectory' do
+    it 'correctly draws the trajectory grid' do
+      probe = Probe.new([0, 0], [1, 1], [4, 5, -3, -2])
+      probe.trajectory = [[0, 0], [1, 1], [2, 0], [3, -1], [4, -2], [5, -3]]
+      grid = probe.visualize_trajectory
+      expected =
+        [
+          '.*....',
+          '*.*...',
+          '...*..',
+          '....*T',
+          '....T*'
+        ]
+      expect(grid).to eq(expected)
     end
   end
 end
