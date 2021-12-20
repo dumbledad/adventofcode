d9394658dfeff353989063b7a54b58ab0f6f412f
Tim Regan <tim.regan@raspberrypi.org>
2021-12-20 08:25:37 +0000

    All points transformed

--- day19.rb
+++ day19.rb
@@ -9,7 +9,7 @@ class Sensors
     lines = File.readlines(input_data_filename).map(&:chomp)
     lines.each do |l|
       if line.start_with? '---'
-        sensor.do_transformations unless sensor.nil? || sensor.ordinal.zero?
+        sensor.do_all_transformations unless sensor.nil? || sensor.ordinal.zero?
         sensor&.build_vectors
         digit = line.match(/\d+/)[0]
         sensor = Sensor.new(digit)
@@ -25,25 +25,51 @@ class Sensor
 
   attr_accessor :name, :beacons
 
+  # https://en.wikipedia.org/wiki/Rotation_matrix
+  NINETY_AROUND_X_CCW = Matrix[[1, 0, 0], [0, 0, -1], [0, 1, 0]]
+  NINETY_AROUND_Y_CCW = Matrix[[0, 0, 1], [0, 1, 0], [-1, 0, 0]]
+  NINETY_AROUND_Z_CCW = Matrix[[0, -1, 0], [1, 0, 0], [0, 0, 1]]
+  REFLECT_IN_YZ = Matrix[[-1, 0, 0], [0, 1, 0], [0, 0, 1]]
+  REFLECT_IN_XZ = Matrix[[1, 0, 0], [0, -1, 0], [0, 0, 1]]
+  REFLECT_IN_XY = Matrix[[1, 0, 0], [0, 1, 0], [0, 0, -1]]
+
   def initialize(name)
     @name = name
     @beacons = { 'x0' => [] }
-    @ninety_around_x_ccw = Matrix[[1, 0, 0], [0, 0, -1], [0, 1, 0]]
   end
 
   # We assume Scanner 0 is correct, and that each other scanner "could be in any of 24 different orientations: facing positive or negative x, y, or z, and considering
   # any of four directions "up" from that facing."
-  def do_transformations
-    # No-op.
+  def do_all_transformations
+    do_rotations('x', NINETY_AROUND_X_CCW, @beacons['x0'])
+    do_reflection('x', REFLECT_IN_YZ, @beacons['x0'])
+    do_rotations('-x', NINETY_AROUND_X_CCW, @beacons['-x0'])
+
+    do_rotations('y', NINETY_AROUND_Y_CCW, @beacons['x0'])
+    do_reflection('y', REFLECT_IN_XZ, @beacons['x0'])
+    do_rotations('-y', NINETY_AROUND_X_CCW, @beacons['-y0'])
+
+    do_rotations('z', NINETY_AROUND_Z_CCW, @beacons['x0'])
+    do_reflection('z', REFLECT_IN_XY, @beacons['x0'])
+    do_rotations('-z', NINETY_AROUND_Z_CCW, @beacons['-z0'])
+
+    # TODO: I can see 18 possible transformations, @ericwastl says it is 24.
   end
 
-  # https://en.wikipedia.org/wiki/Rotation_matrix
-  def do_rotations(axis, beacons)
+  def do_rotations(axis, matrix, beacons)
     new_beacons = beacons.clone
     %w[90 180 270].each do |r_str|
-      new_beacons = new_beacons.map { |b| (@ninety_around_x_ccw * Vector[*b]).to_a }
+      new_beacons = new_beacons.map { |b| transform_beacon(matrix, b) }
       @beacons["#{axis}#{r_str}"] = new_beacons
     end
+  end
+
+  def do_reflection(axis, matrix, beacons)
+    @beacons["-#{axis}0"] = beacons.map { |b| transform_beacon(matrix, b) }
+  end
+
+  def transform_beacon(matrix, beacon)
+    (matrix * Vector[*beacon]).to_a
   end
 
   def build_vectors
--- spec/ruby19_spec.rb
+++ spec/ruby19_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe Sensor do
         ]
       sensor.beacons['x0'] = beacons
       beacons_x0_before = sensor.beacons['x0'].clone
-      sensor.do_rotations('x', beacons)
+      sensor.do_rotations('x', Sensor::NINETY_AROUND_X_CCW, beacons)
       expect(sensor.beacons.keys).to include(*%w[x0 x90 x180 x270])
       expect(sensor.beacons.keys).not_to include(*%w[y0 y90 y180 y270 z0 z90 z180 z270])
       expect(sensor.beacons['x0']).to eq(beacons_x0_before)
