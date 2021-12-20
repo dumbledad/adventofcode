c53b3779e45c38e2044596dfd3c3e59071e60174
Tim Regan <tim.regan@raspberrypi.org>
2021-12-20 07:44:53 +0000

    Testing rotations

--- day19.rb
+++ day19.rb
@@ -1,3 +1,56 @@
 # frozen_string_literal: true
 
 # https://adventofcode.com/2021/day/19
+class Sensors
+  attr_accessor :sensors
+
+  def initialize
+    @sensors = []
+    lines = File.readlines(input_data_filename).map(&:chomp)
+    lines.each do |l|
+      if line.start_with? '---'
+        sensor.do_transformations unless sensor.nil? || sensor.ordinal.zero?
+        sensor&.build_vectors
+        digit = line.match(/\d+/)[0]
+        sensor = Sensor.new(digit)
+        @sensors << sensor
+      end
+      sensor.beacons['x0'] << line.split(',').map(&:to_i) if line.length.positive?
+    end
+  end
+end
+
+class Sensor
+  require 'matrix'
+
+  attr_accessor :name, :beacons
+
+  def initialize(name)
+    @name = name
+    @beacons = { 'x0' => [] }
+    @ninety_around_x_ccw = Matrix[[1, 0, 0], [0, 0, -1], [0, 1, 0]]
+  end
+
+  # We assume Scanner 0 is correct, and that each other scanner "could be in any of 24 different orientations: facing positive or negative x, y, or z, and considering
+  # any of four directions "up" from that facing."
+  def do_transformations
+    # No-op.
+  end
+
+  # https://en.wikipedia.org/wiki/Rotation_matrix
+  def do_rotations(axis, beacons)
+    new_beacons = beacons.clone
+    %w[90 180 270].each do |r_str|
+      new_beacons = new_beacons.map { |b| (@ninety_around_x_ccw * Vector[*b]).to_a }
+      @beacons["#{axis}#{r_str}"] = new_beacons
+    end
+  end
+
+  def build_vectors
+    # No-op.
+  end
+end
+
+def report(filename)
+  # lines = File.read_
+end
--- spec/ruby19_spec.rb
+++ spec/ruby19_spec.rb
@@ -3,3 +3,39 @@
 require './day19'
 
 # https://adventofcode.com/2021/day/19
+RSpec.describe Sensor do
+  describe '#do_rotations' do
+    it 'correctly rotates a beacon around the x axis' do
+      sensor = Sensor.new('0')
+      beacons =
+        [
+          [0.5, 0.5, 0.5],
+          [-0.5, 0.5, 0.5],
+          [0.5, -0.5, 0.5],
+          [0.5, 0.5, -0.5],
+          [-0.5, -0.5, 0.5],
+          [-0.5, 0.5, -0.5],
+          [0.5, -0.5, -0.5],
+          [-0.5, -0.5, -0.5]
+        ]
+      sensor.beacons['x0'] = beacons
+      beacons_x0_before = sensor.beacons['x0'].clone
+      sensor.do_rotations('x', beacons)
+      expect(sensor.beacons.keys).to include(*%w[x0 x90 x180 x270])
+      expect(sensor.beacons.keys).not_to include(*%w[y0 y90 y180 y270 z0 z90 z180 z270])
+      expect(sensor.beacons['x0']).to eq(beacons_x0_before)
+      expect(sensor.beacons['x90']).to eq(
+        [
+          [0.5, -0.5, 0.5],
+          [-0.5, -0.5, 0.5],
+          [0.5, -0.5, -0.5],
+          [0.5, 0.5, 0.5],
+          [-0.5, -0.5, -0.5],
+          [-0.5, 0.5, 0.5],
+          [0.5, 0.5, -0.5],
+          [-0.5, 0.5, -0.5]
+        ]
+      )
+    end
+  end
+end
