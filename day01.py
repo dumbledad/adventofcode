b52dcad438fae1fc9aaa20f1f648a00a4be4d462
Tim Regan <tim.regan@raspberrypi.org>
2022-12-01 08:46:56 +0000

    Without numpy

--- day01.py
+++ day01.py
@@ -1,27 +1,24 @@
 import csv
 import sys
-import numpy as np
 
 class Calories:
   """Store the calorie count of each elf"""
+  elves = []
   
   def __init__(self, filename):
     with open(filename, newline='') as csv_file:
       count = 0
-      elves = []
       reader = csv.reader(csv_file, quoting=csv.QUOTE_NONNUMERIC)
       for row in reader:
         if len(row) == 0:
-          elves.append(count)
+          self.elves.append(count)
           count = 0
         else:
-          count += int(row[0])
-    self.elves = np.array(elves)         
+          count += int(row[0])           
 
 def main(filename):
   calories = Calories(filename)
-  argmax = np.argmax(calories.elves)
-  print(f'Part 1: elf {argmax + 1} is carrying {calories.elves[argmax]} calories')
+  print(f'Part 1: top elf is carrying {max(calories.elves)} calories')
 
 if __name__ == "__main__":
   # Call `python day_1.py <csv_file>`
