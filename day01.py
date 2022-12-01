d81376aafe2eef6ffb86ca9f3419a1cd4166cc47
Tim Regan <tim.regan@raspberrypi.org>
2022-12-01 09:06:49 +0000

    nlargest then sum giving wrong answer?

--- day01.py
+++ day01.py
@@ -1,5 +1,6 @@
 import csv
 import sys
+import pandas as pd
 
 class Calories:
   """Store the calorie count of each elf"""
@@ -15,12 +16,15 @@ class Calories:
           count = 0
         else:
           count += int(row[0])
+          
+  def sum_max(self, top_count):
+    return pd.Series(self.elves).nlargest(top_count).sum()
 
-  def sum_max()         
 
 def main(filename):
   calories = Calories(filename)
   print(f'Part 1: top elf is carrying {max(calories.elves)} calories')
+  print(f'Part 2: the top three elves are carrying {calories.sum_max(3)} calories')
 
 if __name__ == "__main__":
   # Call `python day_1.py <csv_file>`
