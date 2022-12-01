d91472aac9addccc3b422957812f2fd8474e272c
Tim Regan <tim.regan@raspberrypi.org>
2022-12-01 08:29:06 +0000

    Working on Part 1 test data

--- day01.py
+++ day01.py
@@ -4,22 +4,24 @@ import numpy as np
 
 class Calories:
   """Store the calorie count of each elf"""
-  elves = np.array()
   
   def __init__(self, filename):
     with open(filename, newline='') as csv_file:
       count = 0
+      elves = []
       reader = csv.reader(csv_file, quoting=csv.QUOTE_NONNUMERIC)
       for row in reader:
-        if row == '':
-          self.elves.append(count)
+        if len(row) == 0:
+          elves.append(count)
         else:
-          count += int(row)          
+          count += int(row[0])
+    self.elves = np.array(elves)         
 
 def main(filename):
   calories = Calories(filename)
-  print(f'')
+  print(f'Part 1: {np.argmax(calories.elves) + 1}')
 
 if __name__ == "__main__":
   # Call `python day_1.py <csv_file>`
   main(sys.argv[1])
+  
