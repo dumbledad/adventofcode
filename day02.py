1c48c8748b429b64060e0f684011d8172e98f79b
dumbledad <tim.regan@raspberrypi.org>
2022-12-02 13:54:28 +0000

    Part 2 done

--- day02.py
+++ day02.py
@@ -4,11 +4,57 @@ import pandas as pd
 
 class RockPaperScissors:  
   def __init__(self, filename):
-    elf = {
+    self.elf = {
       'A': 'Rock', 
       'B': 'Paper',
       'C': 'Scissors',
     }
+    self.shape_score = {
+      'Rock': 1,
+      'Paper': 2,
+      'Scissors': 3,
+    }
+    self.outcome_score = {
+      'win': 6,
+      'draw': 3,
+      'lose': 0,
+    }
+    self.score = 0
+    with open(filename, newline='') as csv_file:
+      reader = csv.reader(csv_file, delimiter=' ')
+      for row in reader:
+        if len(row) > 0:
+          self.part2_score(row)
+  
+  def part2_score(self, row):
+    round_outcome = {
+      'X': 'lose', 
+      'Y': 'draw',
+      'Z': 'win',
+    }
+    shape_choice = {
+      'lose': {
+        'Rock': 'Scissors',
+        'Paper': 'Rock',
+        'Scissors': 'Paper',
+      },
+      'draw': {
+        'Rock': 'Rock',
+        'Paper': 'Paper',
+        'Scissors': 'Scissors',
+      },
+      'win': {
+        'Rock': 'Paper',
+        'Paper': 'Scissors',
+        'Scissors': 'Rock',
+      },
+    }
+    elf_shape = self.elf[row[0]]
+    my_shape = shape_choice[round_outcome[row[1]]][elf_shape]
+    self.score += self.shape_score[my_shape]
+    self.score += self.outcome_score[round_outcome[row[1]]]
+    
+  def part1_score(self, row):
     me = {
       'X': 'Rock', 
       'Y': 'Paper',
@@ -31,28 +77,12 @@ class RockPaperScissors:
         'Scissors': 'draw',
       },
     }
-    shape_score = {
-      'Rock': 1,
-      'Paper': 2,
-      'Scissors': 3,
-    }
-    outcome_score = {
-      'win': 6,
-      'draw': 3,
-      'lose': 0,
-    }
-    self.score = 0
-    with open(filename, newline='') as csv_file:
-      reader = csv.reader(csv_file, delimiter=' ')
-      for row in reader:
-        if len(row) > 0:
-          elf_shape = elf[row[0]]
-          my_shape = me[row[1]]
-          round_outcome = outcome[my_shape][elf_shape]
-          self.score += shape_score[my_shape]
-          self.score += outcome_score[round_outcome]
-          # print(f'{row} [{elf_shape}, {my_shape}] : {round_outcome} : {shape_score[my_shape]} + {outcome_score[round_outcome]} = {shape_score[my_shape] + outcome_score[round_outcome]}, total score is {self.score}')
-          
+    elf_shape = self.elf[row[0]]
+    my_shape = me[row[1]]
+    round_outcome = outcome[my_shape][elf_shape]
+    self.score += self.shape_score[my_shape]
+    self.score += self.outcome_score[round_outcome]
+   
 def main(filename):
   game = RockPaperScissors(filename)
   print(f'My score is {game.score}')
