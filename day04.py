733bc50a8737e986d5eadc5165d8e6231fc5afeb
dumbledad <tim.regan@raspberrypi.org>
2022-12-04 08:26:34 +0000

    Refactor _ private

--- day04.py
+++ day04.py
@@ -7,11 +7,12 @@ class Assignments:
       reader = csv.reader(csv_file)
       for row in reader:
         if len(row) > 0:
-          self.pairs.append(self.parse_row(row))
+          self.pairs.append(self._parse_row(row))
 
-  def parse_row(self, row):
-    return [self.parse_range(row[0]), self.parse_range(row[1])]
+  def _parse_row(self, row):
+    return [self._parse_range(row[0]), self._parse_range(row[1])]
 
-  def parse_range(self, range_string):
+  def _parse_range(self, range_string):
     range_elements = range_string.split('-')
     return range(int(range_elements[0]), int(range_elements[1]) + 1)
+  
