b38fb008292df8a41757aba8fb4ccb59df37f873
dumbledad <tim.regan@raspberrypi.org>
2022-12-04 08:49:57 +0000

    Part 2 done

--- day04.py
+++ day04.py
@@ -9,6 +9,13 @@ class Assignments:
     intersection = set1.intersection(set2)
     return intersection == set1 or intersection == set2
 
+  @classmethod
+  def has_overlap(cls, range1, range2):
+    set1 = set(range1)
+    set2 = set(range2)
+    intersection = set1.intersection(set2)
+    return len(intersection) > 0
+
   def __init__(self, filename):
     self.pairs = []
     with open(filename, newline='') as csv_file:
@@ -20,6 +27,10 @@ class Assignments:
   @cached_property
   def containment_count(self):
     return sum([1 if Assignments.has_containment(ranges[0], ranges[1]) else 0 for ranges in self.pairs])
+  
+  @cached_property
+  def overlap_count(self):
+    return sum([1 if Assignments.has_overlap(ranges[0], ranges[1]) else 0 for ranges in self.pairs])
 
   def _parse_row(self, row):
     return [self._parse_range(row[0]), self._parse_range(row[1])]
@@ -27,4 +38,12 @@ class Assignments:
   def _parse_range(self, range_string):
     range_elements = range_string.split('-')
     return range(int(range_elements[0]), int(range_elements[1]) + 1)
-  
+
+
+def main():
+  assignments = Assignments('day04.csv')
+  print(f'Part 1: {assignments.containment_count}')
+  print(f'Part 2: {assignments.overlap_count}')
+
+if __name__ == "__main__":
+  main()
--- test_day04.py
+++ test_day04.py
@@ -13,3 +13,10 @@ def test_assignments_has_containment():
 def test_assignments_containment_count():
   assignments = Assignments('day04-test.csv')
   assert assignments.containment_count == 2
+
+def test_assignments_has_overlap():
+  assert Assignments.has_overlap(range(4, 9), range(5, 12))
+  assert Assignments.has_overlap(range(5, 12), range(4, 9))
+  assert Assignments.has_overlap(range(5, 9), range(4, 9))
+  assert not Assignments.has_overlap(range(5, 10), range(11, 19))
+  
