5f5f27371ffa6a9b6aa9e7f5523852a544d90621
dumbledad <tim.regan@raspberrypi.org>
2022-12-06 06:42:10 +0000

    Tests done for part 2

--- day06.py
+++ day06.py
@@ -1,8 +1,15 @@
 class Tuning:
   @classmethod
-  def start_of_packet(cls, datastream):
+  def start_of_packet(cls, datastream, marker_count=4):
     data = list(datastream)
     for i, _ in enumerate(data):
-      if i > 3 and len(set(data[i-4:i])) == 4:
+      if i > marker_count - 1 and len(set(data[i-marker_count:i])) == marker_count:
         return i
     return -1
+
+
+def main():
+  print(Tuning.start_of_packet(open('day06.txt').read()))
+
+if __name__ == "__main__":
+  main()
--- test_day06.py
+++ test_day06.py
@@ -1,13 +1,17 @@
 from day06 import Tuning
 
 datastreams = [
-  { 'datastream': 'mjqjpqmgbljsphdztnvjfqwrcgsmlb', 'position': 7 },
-  { 'datastream': 'bvwbjplbgvbhsrlpgdmjqwftvncz', 'position': 5 },
-  { 'datastream': 'nppdvjthqldpwncqszvftbrmjlhg', 'position': 6 },
-  { 'datastream': 'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 'position': 10 },
-  { 'datastream': 'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 'position': 11 }
+  { 'datastream': 'mjqjpqmgbljsphdztnvjfqwrcgsmlb', 'position': 7, 'message': 19 },
+  { 'datastream': 'bvwbjplbgvbhsrlpgdmjqwftvncz', 'position': 5, 'message': 23 },
+  { 'datastream': 'nppdvjthqldpwncqszvftbrmjlhg', 'position': 6, 'message': 23 },
+  { 'datastream': 'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 'position': 10, 'message': 29 },
+  { 'datastream': 'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 'position': 11, 'message': 26 }
 ]
 
 def test_start_of_packet():
   for pair in datastreams:
     assert Tuning.start_of_packet(pair['datastream']) == pair['position']
+
+def test_start_of_message():
+  for pair in datastreams:
+    assert Tuning.start_of_packet(pair['datastream'], marker_count=14) == pair['message']
