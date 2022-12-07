699b9ae7db49a47112abd49ae45f150249dffe49
dumbledad <tim.regan@raspberrypi.org>
2022-12-07 12:46:06 +0000

    all_sub_dirs test works

--- day07.py
+++ day07.py
@@ -5,6 +5,8 @@ class File:
     self.name = name
     self.size = size
 
+
+
 class Dir:
   def __init__(self, name, parent=None):
     self.name = name
@@ -14,7 +16,7 @@ class Dir:
 
   def add_file(self, file_name, file_size):
     if not [file for file in self._files if file.name == file_name]:
-      self._files.append(File(file_name, file_size))
+      self._files.append(File(file_name, int(file_size)))
 
   def cd(self, dir_name):
     sub_dir =  next((sub_dir for sub_dir in self._sub_dirs if sub_dir.name == dir_name), None)
@@ -27,9 +29,26 @@ class Dir:
     if self._parent:
       return self._parent
     raise Exception('Cannot .. from root directory')
+  
+  @property
+  def size(self):
+    size = sum([file.size for file in self._files]) + sum([sub_dir.size for sub_dir in self._sub_dirs])
+    return size
+  
+  def all_sub_dirs(self, called_by=''):
+    dirs = [] + self._sub_dirs
+    for i, sub_dir in enumerate(self._sub_dirs):
+      dirs += sub_dir.all_sub_dirs(called_by + self.name)
+    return dirs
+
+  def sum_size_under_count_once(self, under):
+    files_size = sum([file.size for file in self._files])
+    size = files_size if files_size <= under else 0
+    size += sum([sub_dir.sum_size_under_count_once(under) for sub_dir in self._sub_dirs])
+    return size
 
   def sum_size_under(self, under):
-    return sum([file.size for file in self._files if file.size <= under])
+    return sum([sub_dir.size for sub_dir in self.all_sub_dirs if sub_dir.size <= under])
 
 
 class Filesystem:
@@ -37,12 +56,12 @@ class Filesystem:
   def parse_dir_tree(cls, rows):
     root = cwd = Dir('root')
     for row in rows:
+      if row == '$ cd ..':
+        cwd = cwd.up()
+        continue
       cd_match = re.match('\$ cd (.+)', row)
       if cd_match:
         cwd = cwd.cd(cd_match.group(1))
-        continue
-      if row == '$ cd ..':
-        cwd = cwd.up()
         continue
       file_match = re.match('(\d+) (.*)', row)
       if file_match:
@@ -53,9 +72,6 @@ class Filesystem:
     with open(filename) as data:
       self.data = data.read()
       self.root = Filesystem.parse_dir_tree(self.data.splitlines())
-
-  def file_sum(self, under):
-    return sum([int(num.strip()) for num in re.findall('\\n(\d+)', self.data) if int(num.strip()) <= under])
 
 
 def main():
--- test_day07.py
+++ test_day07.py
@@ -1,9 +1,21 @@
 from day07 import Filesystem
 
-# def test_filesystem_file_sum():
-#   fs = Filesystem('day07-test.txt')
-#   assert fs.file_sum(100_000) == 94_853
-
 def test_filesystem_init():
   fs = Filesystem('day07-test.txt')
-  assert fs.data
+  assert fs.root._sub_dirs[0].name == '/'
+
+def test_dir_all_sub_dirs():
+  fs = Filesystem('day07-test.txt')
+  assert len(fs.root.all_sub_dirs()) == 4
+
+def test_dir_sum_size_under():
+  fs = Filesystem('day07-test.txt')
+  assert fs.root.sum_size_under(100_000) == 95_437
+
+def test_dir_sum_size_under_count_once():
+  fs = Filesystem('day07-test.txt')
+  assert fs.root.sum_size_under_count_once(100_000) == 94_853
+
+def test_dir_size():
+  fs = Filesystem('day07-test.txt')
+  assert fs.root.size == 48_381_165
