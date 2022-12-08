ade83cc0b7701b2e9bd71e101850632d79cea1fb
dumbledad <tim.regan@raspberrypi.org>
2022-12-08 08:42:26 +0000

    _higher fails test

--- day08.py
+++ day08.py
@@ -3,3 +3,19 @@ class Trees:
     with open(filename) as file:
       self.data = file.read()
       self.grid = [list(line) for line in self.data.splitlines()]
+
+  @property
+  def visible_tree_count(self):
+    count == 0
+    for i, row in self.grid:
+      for j, height in row:
+        if i == 0 or i == len(self.grid) - 1 or j == 0 or j == len(row) - 1:
+          count += 1
+          continue
+
+  def _higher(self, i, j):
+    return self.grid[i][j] > self.grid[i - 1][j] and \
+           self.grid[i][j] > self.grid[i + 1][j] and \
+           self.grid[i][j] > self.grid[i][j - 1] and \
+           self.grid[i][j] > self.grid[i][j + 1]                         
+        
--- test_day08.py
+++ test_day08.py
@@ -4,3 +4,8 @@ def test_trees_init():
   trees = Trees('day08-test.txt')
   assert trees.data.count('\n') == 5
   assert len(trees.grid) == len(trees.grid[0]) == 5
+
+def test_trees_higher():
+  trees = Trees('day08-test.txt')
+  assert trees._higher(1, 1)
+  assert not trees._higher(2, 2)
