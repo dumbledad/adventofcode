2c63b941dc1fcae8674ad82416ddc33ea7dc1593
dumbledad <tim.regan@raspberrypi.org>
2022-12-08 12:57:27 +0000

    Part 1 tests pass

--- day08.py
+++ day08.py
@@ -6,16 +6,39 @@ class Trees:
 
   @property
   def visible_tree_count(self):
-    count == 0
-    for i, row in self.grid:
-      for j, height in row:
+    count = 0
+    for i, row in enumerate(self.grid):
+      for j, _ in enumerate(row):
         if i == 0 or i == len(self.grid) - 1 or j == 0 or j == len(row) - 1:
           count += 1
           continue
+        if self._higher_on_a_sight_line(i, j):
+          count += 1
+    return count
 
-  def _higher(self, i, j):
-    return self.grid[i][j] > self.grid[i - 1][j] and \
-           self.grid[i][j] > self.grid[i + 1][j] and \
-           self.grid[i][j] > self.grid[i][j - 1] and \
-           self.grid[i][j] > self.grid[i][j + 1]                         
+  def _higher_than_a_neighbour(self, i, j):
+    return self.grid[i][j] > self.grid[i - 1][j] or \
+           self.grid[i][j] > self.grid[i + 1][j] or \
+           self.grid[i][j] > self.grid[i][j - 1] or \
+           self.grid[i][j] > self.grid[i][j + 1]
+
+  def _higher_on_a_sight_line(self, i, j):
+    direction_count = 0
+    for idx_i in range(0, i):
+      if self.grid[idx_i][j] >= self.grid[i][j]:
+        direction_count += 1
+        break
+    for idx_i in range(len(self.grid) - 1, i, -1):
+      if self.grid[idx_i][j] >= self.grid[i][j]:
+        direction_count += 1
+        break
+    for idx_j in range(0, j):
+      if self.grid[i][idx_j] >= self.grid[i][j]:
+        direction_count += 1
+        break
+    for idx_j in range(len(self.grid[i]) - 1, j, -1):
+      if self.grid[i][idx_j] >= self.grid[i][j]:
+        direction_count += 1
+        break
+    return direction_count < 4         
         
--- test_day08.py
+++ test_day08.py
@@ -5,7 +5,19 @@ def test_trees_init():
   assert trees.data.count('\n') == 5
   assert len(trees.grid) == len(trees.grid[0]) == 5
 
-def test_trees_higher():
+def test_trees_higher_than_a_neighbour():
   trees = Trees('day08-test.txt')
-  assert trees._higher(1, 1)
-  assert not trees._higher(2, 2)
+  assert trees._higher_than_a_neighbour(1, 1)
+  assert not trees._higher_than_a_neighbour(2, 2)
+
+def test_higher_on_a_sight_line():
+  trees = Trees('day08-test.txt')
+  assert trees._higher_on_a_sight_line(1, 1)
+  assert not trees._higher_on_a_sight_line(2, 2)
+  # The difference:
+  assert trees._higher_than_a_neighbour(3, 3)
+  assert not trees._higher_on_a_sight_line(3, 3)
+
+def test_visible_tree_count():
+  trees = Trees('day08-test.txt')
+  assert trees.visible_tree_count == 21
