d62a37cad21cc81bb51608f4e2ba5958aabff4d2
Tim Regan <tim.regan@raspberrypi.org>
2022-12-11 07:21:22 +0000

    Troupe tested

--- 2022/test_day11.py
+++ 2022/test_day11.py
@@ -8,3 +8,8 @@ def test_troupe_init():
   troupe = Troupe('inputs/2022/day11-test.txt')
   assert troupe.data
   assert len(troupe.monkeys) == 4
+  assert troupe.monkeys[3].items == [74]
+  assert troupe.monkeys[2].operation == 'new = old * old'
+  assert troupe.monkeys[1].test['divisor'] == 19
+  assert troupe.monkeys[0].test['true'] == 2
+  assert troupe.monkeys[0].test['false'] == 3
