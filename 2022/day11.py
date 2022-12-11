eb4e47f48969d74db245f4e461448deba9b34069
Tim Regan <tim.regan@raspberrypi.org>
2022-12-11 07:16:50 +0000

    Simple troupe testing

--- 2022/day11.py
+++ 2022/day11.py
@@ -17,12 +17,37 @@ class Monkey:
     return thrown
 
   def inspect(self, item):
-    return self.operation(item)
+    vars = { 'old': item }
+    exec(self.operation, vars)
+    return vars['new']
 
   def throw_to(self, item):
-    return self.test(item)
+    return self.test['true'] if item % self.test['divisor'] == 0 else self.test['false']
 
 
 class Troupe:
-  def __input__(self, filename):
-    return -1
+  def __init__(self, filename):
+    monkey_txt = 'Monkey '
+    starting_items_txt = '  Starting items: '
+    operation_txt = '  Operation: '
+    divisible_test_txt = '  Test: divisible by '
+    true_txt = '    If true: throw to monkey '
+    false_txt = '    If false: throw to monkey '
+    self.monkeys = []
+    current_monkey = {}
+    with open(filename) as file:
+      self.data = file.read()
+    for line in self.data.splitlines():
+      if line.startswith(starting_items_txt):
+        current_monkey['starting_items'] = list([int(item) for item in line.replace(starting_items_txt, '').split(',')])
+      elif line.startswith(operation_txt):
+        current_monkey['operation'] = line.replace(operation_txt, '')
+      elif line.startswith(divisible_test_txt):
+        current_test = { 'divisor': int(line.replace(divisible_test_txt, '')) }
+      elif line.startswith(true_txt):
+        current_test['true'] = int(line.replace(true_txt, ''))
+      elif line.startswith(false_txt):
+        current_test['false'] = int(line.replace(false_txt, ''))
+        current_monkey['test'] = current_test
+        self.monkeys.append(Monkey(**current_monkey))
+        current_monkey = {}
--- 2022/test_day11.py
+++ 2022/test_day11.py
@@ -3,3 +3,8 @@ from day11 import Troupe, Monkey
 def test_monkey_get_bored():
   assert Monkey.bored_with(601) == 200
   assert Monkey.bored_with(602) == 201
+
+def test_troupe_init():
+  troupe = Troupe('inputs/2022/day11-test.txt')
+  assert troupe.data
+  assert len(troupe.monkeys) == 4
