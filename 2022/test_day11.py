734e30ff57b1570fcc4cf3acdc311a354a7dc553
Tim Regan <tim.regan@raspberrypi.org>
2022-12-11 19:15:58 +0000

    Compile in test too

--- 2022/test_day11.py
+++ 2022/test_day11.py
@@ -5,7 +5,7 @@ def test_troupe_init():
   assert troupe.data
   assert len(troupe.monkeys) == 4
   assert troupe.monkeys[3].items == [74]
-  assert troupe.monkeys[2].operation == 'new = old * old'
+  assert troupe.monkeys[2].operation == compile('new = old * old', '<string>', 'exec')
   assert troupe.monkeys[1].test['divisor'] == 19
   assert troupe.monkeys[0].test['true'] == 2
   assert troupe.monkeys[0].test['false'] == 3
