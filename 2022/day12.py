784b206976264f3236d767e16b4184d213085abf
Tim Regan <tim.regan@raspberrypi.org>
2022-12-12 07:45:25 +0000

    Start is path length 0

--- 2022/day12.py
+++ 2022/day12.py
@@ -1,22 +1,38 @@
 import sys
 
 class Hill:
-  def __init__(self, height):
+  def __init__(self, height, i, j):
     self.visited = False
-    self.tentative_distance = sys.maxsize
-    self.start = False
-    self.end = False
+    self.position = (i, j)
     match height:
       case 'S':
         self.height = ord('a')
+        self.tentative_distance = 0
         self.start = True
+        self.end = False
       case 'E':
         self.height = ord('z')
+        self.tentative_distance = sys.maxsize
+        self.start = False
         self.end = True
       case _:
         self.height = ord(height)
+        self.tentative_distance = sys.maxsize
+        self.start = False
+        self.end = False
 
 class Dijkstra:
   def __init__(self, filename):
     with open(filename) as file:
       self.data = file.read()
+    self.visited = list
+    self.unvisited = list
+    for i, line in enumerate(self.data.splitlines()):
+      for j, hill_char in enumerate(list(line)):
+        self.unvisited.append(Hill(hill_char, i, j))
+
+  def visit(self, hill):
+    return -1
+
+  def neighbours(self, hill):
+    return -1
--- 2022/test_day12.py
+++ 2022/test_day12.py
@@ -4,17 +4,17 @@ import sys
 filename = 'inputs/2022/day12-test.txt'
 
 def test_hill_init():
-  hill = Hill('S')
+  hill = Hill('S', 0, 0)
   assert hill.height == ord('a')
   assert hill.start
   assert not hill.end
-  assert hill.tentative_distance == sys.maxsize
-  hill = Hill('E')
+  assert hill.tentative_distance == 0
+  hill = Hill('E', 4, 6)
   assert hill.height == ord('z')
   assert not hill.start
   assert hill.end
   assert hill.tentative_distance == sys.maxsize
-  hill = Hill('g')
+  hill = Hill('g', 3, 3)
   assert hill.height == ord('g')
   assert not hill.start
   assert not hill.end
