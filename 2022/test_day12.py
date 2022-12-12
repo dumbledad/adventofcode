9d24d3b0c1906c19179bd062e4beb66d3bcb359b
Tim Regan <tim.regan@raspberrypi.org>
2022-12-12 07:34:48 +0000

    maxsize too

--- 2022/test_day12.py
+++ 2022/test_day12.py
@@ -1,4 +1,5 @@
 from day12 import Dijkstra, Hill
+import sys
 
 filename = 'inputs/2022/day12-test.txt'
 
@@ -7,14 +8,17 @@ def test_hill_init():
   assert hill.height == ord('a')
   assert hill.start
   assert not hill.end
+  assert hill.tentative_distance == sys.maxsize
   hill = Hill('E')
   assert hill.height == ord('z')
   assert not hill.start
   assert hill.end
+  assert hill.tentative_distance == sys.maxsize
   hill = Hill('g')
   assert hill.height == ord('g')
   assert not hill.start
   assert not hill.end
+  assert hill.tentative_distance == sys.maxsize
 
 def test_dijkstra_init():
   dijkstra = Dijkstra(filename)
