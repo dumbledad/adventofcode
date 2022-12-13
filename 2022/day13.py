d44798c71d12dc4744573762e31810831a886a80
Tim Regan <tim.regan@raspberrypi.org>
2022-12-13 06:02:14 +0000

    Compare failing test

--- 2022/day13.py
+++ 2022/day13.py
@@ -1,6 +1,29 @@
 import json
 
 class Packets:
+  @classmethod
+  def compare(cls, left, right):
+    if isinstance(left, list) and isinstance(right, int):
+      right = [right]
+    elif isinstance(left, int) and isinstance(right, list):
+      left = [left]
+    if isinstance(left, int) and isinstance(right, int) and left < right:
+      return False
+    if isinstance(left, list) and isinstance(right, list):
+      if len(left) > len(right):
+        return False
+      for i in range(len(left)):
+        if not Packets.compare(left[i], right[i]):
+          return False
+    return True
+
   def __init__(self, filename):
     with open(filename) as file:
       self.data = file.read()
+    self.potentials = []
+    for pair in self.data.split('\n\n'):
+     self.potentials.append(tuple([json.loads(packet_str) for packet_str in pair.splitlines()]))
+
+  @property
+  def right_indexes(self):
+    return [i + 1 for i in range(len(self.potentials)) if Packets.compare(self.potentials[i][0], self.potentials[i][0])]
--- 2022/test_day13.py
+++ 2022/test_day13.py
@@ -5,3 +5,12 @@ filename = 'inputs/2022/day13-test.txt'
 def test_packets_init():
   packets = Packets(filename)
   assert packets.data
+  assert len(packets.potentials) == 8
+
+def test_packets_compare():
+  assert Packets.compare([1,1,3,1,1], [1,1,5,1,1])
+  assert not Packets.compare([[1],[2,3,4]], [[1],4])
+
+def test_packets_right_indexes():
+  packets = Packets(filename)
+  assert list(packets.right_indexes) == [1, 2, 4, 6]
