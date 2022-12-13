cbfd6793591afd66760e0a2ce0a669439b4f6176
Tim Regan <tim.regan@raspberrypi.org>
2022-12-13 08:46:39 +0000

    logging.debug to logging.info

--- 2022/day13.py
+++ 2022/day13.py
@@ -4,25 +4,25 @@ import logging
 class Packets:
   @classmethod
   def compare(cls, left, right, indent=''):
-    logging.debug(f'{indent}Compare {left} vs {right}')
+    logging.info(f'{indent}Compare {left} vs {right}')
     if isinstance(left, list) and isinstance(right, int):
       right = [right]
     elif isinstance(left, int) and isinstance(right, list):
       left = [left]
     if isinstance(left, int) and isinstance(right, int):
       if left < right:
-        logging.debug(f'  {indent} Left side is smaller, so inputs are in the right order')
+        logging.info(f'  {indent} Left side is smaller, so inputs are in the right order')
         return True
       elif left > right:
-        logging.debug(f'  {indent} Right side is smaller, so inputs are not in the right order')
+        logging.info(f'  {indent} Right side is smaller, so inputs are not in the right order')
         return False
     if isinstance(left, list) and isinstance(right, list):
       for i in range(max(len(left), len(right))):
         if i >= len(left):
-          logging.debug(f'  {indent} Left side ran out of items, so inputs are in the right order')
+          logging.info(f'  {indent} Left side ran out of items, so inputs are in the right order')
           return True
         elif i >= len(right):
-          logging.debug(f'  {indent} Right side ran out of items, so inputs are not in the right order')
+          logging.info(f'  {indent} Right side ran out of items, so inputs are not in the right order')
           return False
         match Packets.compare(left[i], right[i], indent=indent+'  '):
           case True:
@@ -38,9 +38,9 @@ class Packets:
       self.data = file.read()
     self.potentials = []
     for i, pair in enumerate(self.data.split('\n\n')):
-      logging.debug(f'== Pair {i + 1} ==')
+      logging.info(f'== Pair {i + 1} ==')
       self.potentials.append(tuple([json.loads(packet_str) for packet_str in pair.splitlines()]))
-      logging.debug('\n')
+      logging.info('\n')
 
   @property
   def correct_indexes(self):
@@ -49,8 +49,6 @@ class Packets:
 def main():
   packets = Packets('inputs/2022/day13-test.txt')
   print(f'Part 1: {sum(packets.correct_indexes)}')
-  # for i in packets.correct_indexes:
-  #   print(f'{i - 1}: {packets.potentials[i - 1]}')
 
 if __name__ == '__main__':
   main()
