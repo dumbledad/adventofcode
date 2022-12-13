4e963b5af4c9495fac978cf850ebdc7dfa393590
Tim Regan <tim.regan@raspberrypi.org>
2022-12-13 07:54:02 +0000

    Reinstate Compare ready for hand crafted tests

--- 2022/test_day13.py
+++ 2022/test_day13.py
@@ -7,6 +7,12 @@ def test_packets_init():
   assert packets.data
   assert len(packets.potentials) == 8
 
+def test_packets_compare():
+  assert Packets.compare([1,1,3,1,1], [1,1,5,1,1])
+  assert Packets.compare([[1],[2,3,4]], [[1],4])
+  assert not Packets.compare([9], [[8,7,6]])
+  assert Packets.compare([[4,4],4,4], [[4,4],4,4,4])
+
 def test_packets_right_indexes():
   packets = Packets(filename)
   assert list(packets.correct_indexes) == [1, 2, 4, 6]
